9574d2b0f4ca64fd55cfc45f89dbb6fca6620bc5
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2023-11-10 12:43:34 +0100

    påbörjat

---
+++ Vinterproj2/Card.cs
@@ -0,0 +1,6 @@
+﻿public class Card
+{
+    public string name;
+    public int value;
+    public bool isDealt = false;
+}
---
+++ Vinterproj2/Deck.cs
@@ -0,0 +1,21 @@
+﻿public class Deck
+{
+    List<Card> cards = new();
+
+    public Deck()
+    {
+        string[] colors = {"clubs", "hearts", "spades", "diamonds"};
+        foreach (var colos in colors)
+        {
+
+            for (int i = 0; i < 13; i++)
+            {
+                Card c1 = new Card();
+                cards.Add(c1);
+            }
+            
+        }
+
+
+    }
+}
